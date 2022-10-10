f24f6f9464add46daf1e9ebbcaab868508b42170
samuelsih <samuel.205025@mhs.its.ac.id>
2022-10-11 02:02:52 +0700

    fix: wrong error and mailer

--- mail/mail.go
+++ mail/mail.go
@@ -2,7 +2,9 @@ package mail
 
 import (
 	"context"
+	"errors"
 	"net/smtp"
+	"sync/atomic"
 	"time"
 
 	"github.com/rs/zerolog/log"
@@ -14,8 +16,7 @@ type Mailer struct {
 	From string
 
 	pool *mail.Pool
-	emailQueue chan *mail.Email
-	maxEmailPerDay int16
+	maxEmailPerDay int32
 	timeout time.Duration
 
 	errChan chan error
@@ -32,14 +33,13 @@ type Message struct {
 func NewMailer(host, address, email, password string) *Mailer {
 	p, err := mail.NewPool(host, 4, smtp.PlainAuth("", email, password, address))
 	if err != nil {
-		log.Fatal().Msg("Error on closing database" + err.Error())
+		log.Fatal().Msg("Error on creating mailer: " + err.Error())
 	}
 
 	mailer := &Mailer{
 		From: email,
 		pool: p, 
-		maxEmailPerDay: 1500, 
-		emailQueue: make(chan *mail.Email),
+		maxEmailPerDay: 0, 
 		timeout: time.Second * 10,
 		doneChan: make(chan struct{}), 
 		errChan: make(chan error),
@@ -55,23 +55,33 @@ func (m *Mailer) Send(ctx context.Context, msg Message) {
 	e.Subject = msg.Subject
 	e.HTML = []byte(msg.HTMLContent)
 
-	m.emailQueue <- e
+	go m.send(ctx, e)
+}
 
-	sendEmail := func() {
-		for e := range m.emailQueue {
-			err := m.pool.Send(e, m.timeout)
-			if err != nil {
-				m.errChan <- err
-			}
-		}
-	}
-
+func (m *Mailer) send(ctx context.Context, msg *mail.Email) {
 	select {
-		case <-ctx.Done():
+	case <-ctx.Done():
+		return
+
+	default:
+		if m.maxEmailPerDay == 1500 {
+			m.errChan <- errors.New("cant send email right now, limit reached")
 			return
-		
-		default:
-			go sendEmail() 
+		}
+
+		if msg == nil {
+			m.errChan <- errors.New("email that want to send is nil")
+			return
+		}
+
+		err := m.pool.Send(msg, m.timeout)
+		if err != nil {
+			m.errChan <- err
+			return
+		}
+
+		atomic.AddInt32(&m.maxEmailPerDay, 1)
+		return
 	}
 }
 
@@ -84,7 +94,7 @@ func (m *Mailer) listen() {
 
 		case err := <-m.errChan:
 			if err != nil {
-				log.Error().Msg("Error on closing database" + err.Error())
+				log.Error().Msg("Error on mailer: " + err.Error())
 			}
 		}
 	}
