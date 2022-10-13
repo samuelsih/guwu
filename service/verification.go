2b929b559bc5d9dcf5dc0db9d86e56e8c0091823
samuelsih <samuel.205025@mhs.its.ac.id>
2022-10-13 23:35:02 +0700

    feat: email content

--- service/verification.go
+++ service/verification.go
@@ -1,8 +1,11 @@
 package service
 
 import (
+	"bytes"
 	"context"
 	"encoding/base64"
+	"html/template"
+	"net/http"
 
 	"github.com/samuelsih/guwu/mail"
 	"golang.org/x/crypto/bcrypt"
@@ -19,11 +22,17 @@ type VerifOut struct {
 func (v *Verification) Send(ctx context.Context, email string) VerifOut {
 	var out VerifOut
 
+	htmlContent, err := buildHTMLMessage(email)
+	if err != nil {
+		out.SetError(http.StatusInternalServerError, err.Error())
+		return out
+	}
+
 	msg := mail.Message{
 		To: email,
 		Subject: "Verification",
 		PlainContent: "ini verif: " + "http://localhost:8080/verif?data=" + generateToken(email),
-		HTMLContent: "</p>ini verif</p>",
+		HTMLContent: htmlContent,
 	}
 
 	v.SendEmail(ctx, msg)
@@ -35,4 +44,20 @@ func (v *Verification) Send(ctx context.Context, email string) VerifOut {
 func generateToken(email string) string {
     hash, _ := bcrypt.GenerateFromPassword([]byte(email), bcrypt.DefaultCost)
     return base64.StdEncoding.EncodeToString(hash)
+}
+
+func buildHTMLMessage(email string) (string, error) {
+	templateToRender := "../mail/verif.gohtml"
+
+	t, err := template.New("email-html").ParseFiles(templateToRender)
+	if err != nil {
+		return "", err
+	}
+
+	var tpl bytes.Buffer
+	if err = t.ExecuteTemplate(&tpl, "link", "http://localhost:8080/verif?data=" + email); err != nil {
+		return "", err
+	}
+
+	return tpl.String(), nil
 }
