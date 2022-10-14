81893486e524bf3580b9ec86cc989e7cd13a6a90
samuelsih <samuel.205025@mhs.its.ac.id>
2022-10-14 23:39:20 +0700

    feat: url parse

--- service/verification.go
+++ service/verification.go
@@ -4,8 +4,11 @@ import (
 	"bytes"
 	"context"
 	"encoding/base64"
+	"errors"
 	"html/template"
 	"net/http"
+	"net/url"
+	"path"
 
 	"github.com/samuelsih/guwu/mail"
 	"golang.org/x/crypto/bcrypt"
@@ -15,12 +18,12 @@ type Verification struct {
 	SendEmail func(ctx context.Context, msg mail.Message)
 }
 
-type VerifOut struct {
+type VerifSendOut struct {
 	CommonResponse
 }
 
-func (v *Verification) Send(ctx context.Context, email string) VerifOut {
-	var out VerifOut
+func (v *Verification) Send(ctx context.Context, email string) VerifSendOut {
+	var out VerifSendOut
 
 	htmlContent, err := buildHTMLMessage(email)
 	if err != nil {
@@ -41,23 +44,53 @@ func (v *Verification) Send(ctx context.Context, email string) VerifOut {
 	return out
 }
 
+type VerifyCheckOut struct {
+	Token string `json:"token"`
+	CommonResponse
+}
+
+func (v *Verification) Check(ctx context.Context, link string) VerifyCheckOut {
+	var out VerifyCheckOut
+
+	token, err := parseLink(link)
+	if err != nil {
+		out.SetError(http.StatusBadRequest, err.Error())
+		return out
+	}
+
+	out.Token = token
+	out.SetOK()
+	return out
+} 
+
 func generateToken(email string) string {
     hash, _ := bcrypt.GenerateFromPassword([]byte(email), bcrypt.DefaultCost)
     return base64.StdEncoding.EncodeToString(hash)
 }
 
 func buildHTMLMessage(email string) (string, error) {
-	templateToRender := "../mail/verif.gohtml"
+	templateToRender := "./mail/verif.gohtml"
 
-	t, err := template.New("email-html").ParseFiles(templateToRender)
+	t, err := template.ParseFiles(templateToRender)
 	if err != nil {
 		return "", err
 	}
 
 	var tpl bytes.Buffer
-	if err = t.ExecuteTemplate(&tpl, "link", "http://localhost:8080/verif?data=" + email); err != nil {
+	if err := t.Execute(&tpl, map[string]string{
+		"link": "http://localhost:8080/verif?data=" + generateToken(email),
+	}); err != nil {
 		return "", err
 	}
 
 	return tpl.String(), nil
+}
+
+func parseLink(link string) (string, error) {
+	u, err := url.Parse(link)
+	if err != nil {
+		return "", errors.New("not a valid url")
+	}
+
+	return path.Base(u.Path), nil
 }
