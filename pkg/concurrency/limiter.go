7f23f67df2bccfa0b373c452365e93932e1afd3a
samuelsih <samuel.205025@mhs.its.ac.id>
2023-01-04 23:40:41 +0700

    feat: limiter for future purposes

---
+++ pkg/concurrency/limiter.go
@@ -0,0 +1,99 @@
+package limiter
+
+import (
+	"context"
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+type Limiter struct {
+	PanicErr error
+
+	worker sync.WaitGroup
+	limit chan struct{}
+	once sync.Once
+}
+
+func New(amount int) *Limiter {
+	if amount <= 0 {
+		return &Limiter{limit: make(chan struct{}, 1)}
+	}
+
+	return &Limiter{limit: make(chan struct{}, amount)}
+}
+
+func (lim *Limiter) Go(f func()) {
+	lim.limit <- struct{}{}
+
+	lim.worker.Add(1)
+
+	go func() {
+		defer func() {
+			<- lim.limit
+			lim.worker.Done()
+		}()
+
+		defer lim.catchPanic()
+
+		f()
+	}()
+}
+
+func (lim *Limiter) GoWithCtx(ctx context.Context, f func()) {
+	if ctx.Err() != nil {
+		return
+	}
+
+	select {
+	case lim.limit <- struct{}{}:
+	
+	case <- ctx.Done():
+		return
+	}
+
+	lim.worker.Add(1)
+
+	go func() {
+		defer func() {
+			<- lim.limit
+			lim.worker.Done()
+		}()
+
+		defer lim.catchPanic()
+
+		f()
+	}()	
+}
+
+func (lim *Limiter) Wait() {
+	lim.worker.Wait()
+}
+
+
+func (lim *Limiter) catchPanic() {
+	if r := recover(); r != nil {
+		lim.once.Do(func() {
+			lim.PanicErr = getPanicValue(r)
+		})
+	}
+}
+
+func getPanicValue(val any) error {
+	switch v := val.(type) {
+	case nil:
+		return nil
+	case string:
+		return fmt.Errorf("panic: %v\n%s", v, getStack())
+	case error:
+		return fmt.Errorf("panic in limiter %w\n%s", v, getStack())
+	default:
+		return fmt.Errorf("unhandle panic: %+v\n%s", v, getStack())
+	}
+}
+
+func getStack() []byte {
+	buf := make([]byte, 64<<10)
+	buf = buf[:runtime.Stack(buf, false)]
+	return buf
+}
---
+++ pkg/concurrency/limiter_test.go
@@ -0,0 +1,60 @@
+package limiter
+
+import (
+	"context"
+	"sync/atomic"
+	"testing"
+	"time"
+)
+
+func TestSimpleLimiter(t *testing.T) {
+	var counter atomic.Int32
+	
+	var limiter = New(5)
+
+	for i := 0; i < 5; i++ {
+		limiter.Go(func() {
+			counter.Add(1)
+		})
+	}
+
+	limiter.Wait()
+
+	if counter.Load() != 5 {
+		t.Fatalf("TestSimpleLimiter - expected 5, got %v", counter.Load())
+	}
+}
+
+func TestLimiterCtx(t *testing.T) {
+	var limiter = New(5)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 50 * time.Millisecond)
+	defer cancel()
+
+	var counter atomic.Int32
+
+	for i := 0; i < 5; i++ {
+		limiter.GoWithCtx(ctx, func() {
+			time.Sleep(3 * time.Second)
+			counter.Add(1)
+		})
+	}
+
+	if counter.Load() != 0 {
+		t.Fatalf("TestLimiterCtx - expected 0, got %v", counter.Load())
+	}
+
+	limiter.Wait()
+}
+
+func TestLimiterPanic(t *testing.T) {
+	var limiter = New(1)
+
+	limiter.Go(func() {
+		panic("foo")
+	})
+
+	if limiter.PanicErr != nil {
+		t.Fatalf("expected panic: foo, got %v", limiter.PanicErr)
+	}
+}	
